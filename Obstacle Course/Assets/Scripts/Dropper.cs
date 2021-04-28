236d7a337fffe6b97a4391e85187d4d10f73c5c7
Jinstarship-Code <yyjh6714@hanmail.net>
2021-04-28 13:43:31 +0900

    Obstacle Course (2)

---
+++ Obstacle Course/Assets/Scripts/Dropper.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    MeshRenderer renderer;
+    Rigidbody rigidbody;
+    [SerializeField] float Time_to_Wait=0f;
+
+    void Start() 
+    {
+        renderer=GetComponent<MeshRenderer>();
+        rigidbody=GetComponent<Rigidbody>();
+
+        renderer.enabled=false;
+        rigidbody.useGravity=false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Time.time>Time_to_Wait)
+       {
+           Debug.Log(Time_to_Wait + " seconds has elapsed");
+            renderer.enabled=true;
+            rigidbody.useGravity=true;
+       }
+
+    }
+}
