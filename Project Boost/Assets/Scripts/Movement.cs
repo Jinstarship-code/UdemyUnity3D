dba6a1362667008fca7ef3f592341838796311ea
Jinstarship-Code <yyjh6714@hanmail.net>
2021-04-29 13:43:59 +0900

    Project Boost 1

---
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float mainThrust=1000f;
+    [SerializeField] float rotationThrust = 20f;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+        ProcessRotation();
+    }
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up*Time.deltaTime*mainThrust);
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }   
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(-Vector3.forward * rotationThisFrame * Time.deltaTime);
+    }
+}
+
+/*
+    AddForce  : World
+    AddRelativeForce : Local
+
+
+*/
