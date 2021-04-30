9d53e75d6339111723263d8504a3e2903ff223bd
Jinstarship-Code <yyjh6714@hanmail.net>
2021-04-30 11:01:30 +0900

    prefix

--- Data/Stack LL/StackLL/main.cpp
+++ Data/Stack LL/StackLL/main.cpp
@@ -73,12 +73,60 @@ int isBalanced(const char *exp)
 		return 0;
 }
 
+int pre(const char x)
+{
+	if (x == '+' || x == '-')
+		return 1;
+	else if (x == '*' || x == '/')
+		return 2;
+	return 0;
+}
+
+int isOperand(const char x)
+{
+	if (x == '+' || x == '-' || x == '*' || x == '/')
+		return 0;
+	else
+		return 1;
+}
+
+char *InToPost(const char *infix)
+{
+	int i = 0, j = 0;
+	char *postfix;
+	int len = strlen(infix);
+	postfix = (char *)malloc((len + 2) * sizeof(char));
+
+	while (infix[i] != '\0')
+	{
+		if (isOperand(infix[i]))
+			postfix[j++] = infix[i++];
+		else
+		{
+			if (pre(infix[i]) > pre(top->data))
+				push(infix[i++]);
+			else
+			{
+				postfix[j++] = pop();
+			}
+		}
+	}
+	while (top != NULL)
+		postfix[j++] = pop();
+	postfix[j] = '\0';
+
+	return postfix;
+}
 
 int main()
 {
-	const char *exp = "{([a+b]*[c-d])}";
+	const char *infix = "a+b*c-d/e";
+	push('#');
 
-	printf("%d ", isBalanced(exp));
+	char *postfix=InToPost(infix);
+
+	printf("%s ", postfix);
+
 
 	return 0;
 }
