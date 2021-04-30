8e5c89e23a8f75d1f54be6c2e2a9e049714803fd
Jinstarship-Code <yyjh6714@hanmail.net>
2021-04-30 13:33:05 +0900

    Project Bosst 2

---
+++ Project Boost/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other) 
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Friendly" :
+                Debug.Log("This thing is friendly");
+                break;
+
+            case "Finish" :
+                LoadNextLevel();
+                break;
+
+            case "Fuel" :
+                Debug.Log("You picked up fuel");
+                break;
+            default :
+                ReloadLevel();
+                break;
+        }
+    }
+    void ReloadLevel()
+    {
+        int currentSceneIndex=SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex=SceneManager.GetActiveScene().buildIndex;
+        int nextScneneIndex=currentSceneIndex+1;
+        if(nextScneneIndex==SceneManager.sceneCountInBuildSettings)
+        {
+            nextScneneIndex=0;
+        }
+        SceneManager.LoadScene(nextScneneIndex);
+    }
+
+
+
+}
--- Project Boost/Assets/Scripts/Movement.cs
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] float mainThrust=1000f;
     [SerializeField] float rotationThrust = 20f;
+    
     Rigidbody rb;
+
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rb=GetComponent<Rigidbody>();        
+        audioSource=GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,6 +31,12 @@ public class Movement : MonoBehaviour
         if(Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up*Time.deltaTime*mainThrust);
+            if(!audioSource.isPlaying)
+                audioSource.Play();
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
 
@@ -43,13 +54,13 @@ public class Movement : MonoBehaviour
 
     void ApplyRotation(float rotationThisFrame)
     {
+        rb.freezeRotation=true; //freezing rotation so we can manually rotate
         transform.Rotate(-Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation=false; //unfreezing rotation so the physics
     }
 }
 
 /*
     AddForce  : World
     AddRelativeForce : Local
-
-
 */
