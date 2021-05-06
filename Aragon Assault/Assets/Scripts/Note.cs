228b6b816c45c41ec5437d4dc147930ffe17bd42
Jinstarship-Code <yyjh6714@hanmail.net>
2021-05-06 13:45:42 +0900

    Aragon 2

---
+++ Aragon Assault/Assets/Scripts/Note.cs
@@ -0,0 +1,8 @@
+﻿/*
+    - Bump map : texture with information on how light catches a shape.
+    - Height map : uses white-to-black scale to show height.
+    - Normal map : uses RGB values to indicate x,y,z facing direction.
+
+
+
+*/
