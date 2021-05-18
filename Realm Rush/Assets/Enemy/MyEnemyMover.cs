c34439eec739d81068cadb9b62f1400274953bd7
Jinstarship-Code <yyjh6714@hanmail.net>
2021-05-18 13:33:42 +0900

    Realm Rush 1

---
+++ Realm Rush/Assets/Enemy/MyEnemyMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyEnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+
+    void Start()
+    {
+        PrintWaypointName();
+    }
+
+    void PrintWaypointName()
+    {
+        foreach(Waypoint waypoint in path)
+        {
+            Debug.Log(waypoint.name);
+        }
+    }
+}
