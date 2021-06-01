170638728acc6aca6c63b755701414a21fc343e2
Jinstarship-Code <yyjh6714@hanmail.net>
2021-06-01 13:44:08 +0900

    Zombie Runner 2

--- Zombie Runner/Assets/Enemy/EnemyAI.cs
+++ Zombie Runner/Assets/Enemy/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -6,17 +7,66 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 10f;
     NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
 
-    // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if(isProvoked)
+        {
+            EngageTarget();
+        }
+        else if(distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if(distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        else if(distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+    private void ChaseTarget()
     {
         navMeshAgent.SetDestination(target.position);
     }
+
+    private void AttackTarget()
+    {
+        Debug.Log(name + "has seeked and is destroying" + target.name);
+    }
+
+
+    void OnDrawGizmosSelected() 
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position,chaseRange);
+    }
 }
+
+/*
+    MonoBehaviour.OnDrawGizmosSelected()
+
+    - Implement OnDrawGizmosSelected to draw a gizmo if the object is selected.
+    - Gizmos are drawn only when the object is selected. 
+    Gizmos are not pickable. This is used to ease setup. 
+
+
+
+*/
---
+++ Zombie Runner/Assets/Player/Weapon.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] float damage = 30f;
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitEffect;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }        
+    }
+
+    private void Shoot()
+    {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            CreateHitImpack(hit);
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward)*hit.distance,Color.yellow);
+            if (target == null)
+                return;
+
+            target.TakeDamage(damage);
+        }
+        else
+            return;
+    }
+
+    private void CreateHitImpack(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffect,hit.point,Quaternion.LookRotation(hit.normal));
+        Destroy(impact, 0.1f);
+    }
+}
